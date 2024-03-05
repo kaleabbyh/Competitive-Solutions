28db74b4f916bf90f941f85627b8c9b59588e187
Kaleab Bayih <kaleabbyh@gmail.com>
2024-03-05 14:49:54 +0300

    Update leap.py

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,7 @@
 def leap_year(year: int):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+
+# def leap_year2(year: int):
+#     return year % 6 == 0 and (year % 100 != 0 or year % 400 == 0)
